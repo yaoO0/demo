c15262ff7cf19466cccdb66f26e9f63a4a661d02
YaoShaoBo <shaoboyao@outlook.com>
2019-04-23 18:25:33 +0800

    add content to User model add User Repository add Test

--- demo/src/main/java/com/yao/demo/model/User.java
+++ demo/src/main/java/com/yao/demo/model/User.java
@@ -1,6 +1,95 @@
 package com.yao.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
 
+@Entity
+public class User implements Serializable {
 
-public class User {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = true, unique = true)
+    private String nickName;
+
+    @Column(nullable = false)
+    private String regTime;
+
+    public User(){
+        super();
+    }
+
+    public User(String nickName, String email, String userName, String password, String regTime){
+        super();
+
+        this.nickName = nickName;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+        this.regTime = regTime;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getRegTime() {
+        return regTime;
+    }
+
+    public void setRegTime(String regTime) {
+        this.regTime = regTime;
+    }
+
 }
